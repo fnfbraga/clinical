66329ce584a72c614d1df471cf821503e5b3eb74
frederico.braga@lyfegen.com <frederico.braga@lyfegen.com>
2021-12-27 18:39:22 +0000

    first commit

---
+++ apps/clinical-data-hub/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'clinical-data-hub',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/clinical-data-hub',
+};
